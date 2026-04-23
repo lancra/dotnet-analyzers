6c3b2278e7f8f72e484e262811408c745cad151a
Lance Craig <lancra@outlook.com>
2026-04-23 05:34:44 -0400

    Add .NET Quality Documentation smoke test

---
+++ tests/Library/DotnetQuality/Documentation/CA1200.cs
@@ -0,0 +1,11 @@
+namespace DotnetAnalyzers.SmokeTests.Library.DotnetQuality.Documentation;
+
+/// <summary>
+/// Type <see cref="T:CA1200"/> contains method <see cref="M:CA1200.M"/>.
+/// </summary>
+public static class CA1200
+{
+    public static void M()
+    {
+    }
+}
