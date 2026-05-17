1fc29ca61f64c84a1fcae1834db2b33f2fe59784
Lance Craig <lancra@outlook.com>
2026-05-17 13:02:23 -0400

    Remove split configuration generation
    Singe usage of a single .globalconfig is sufficient, the ability to
    generate options in a partial .editorconfig has been removed.

--- src/functions/New-AnalyzerConfiguration.ps1
+++ src/functions/New-AnalyzerConfiguration.ps1
@@ -1,8 +1,7 @@
-function New-RuleConfiguration {
+function New-AnalyzerConfiguration {
     [CmdletBinding(SupportsShouldProcess)]
     param (
-        [switch]$IncludeOption,
-        [switch]$IncludeVersion
+        [switch] $IncludeVersion
     )
     begin {
         $severities = @{}
@@ -124,18 +123,16 @@ function New-RuleConfiguration {
 
                 $lines = $ruleSettingLines
 
-                if ($IncludeOption) {
-                    $optionSettingLines = $optionSettings |
-                        Where-Object -Property RuleSet -EQ $category.RuleSet |
-                        ForEach-Object { $optionSettingFormat -f $_.Name, $_.Value, $_.Id }
+                $optionSettingLines = $optionSettings |
+                    Where-Object -Property RuleSet -EQ $category.RuleSet |
+                    ForEach-Object { $optionSettingFormat -f $_.Name, $_.Value, $_.Id }
 
-                    if ($optionSettingLines) {
-                        $lines += ,'' + $optionSettingLines
+                if ($optionSettingLines) {
+                    $lines += ,'' + $optionSettingLines
 
-                        $namingSettingLines = New-NamingOption
-                        if ($namingSettingLines) {
-                            $lines += ,'' + $namingSettingLines
-                        }
+                    $namingSettingLines = New-NamingOption
+                    if ($namingSettingLines) {
+                        $lines += ,'' + $namingSettingLines
                     }
                 }
 
